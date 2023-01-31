e9003a38aa55abbef409a2ac3ce0b3179df2b02e
John Murret <john.murret@hashicorp.com>
2023-01-31 16:05:49 -0700

    add EnterpriseMeta interface

---
+++ acl/enterprise_metadata.go
@@ -0,0 +1,28 @@
+package acl
+
+import "hash"
+
+// EnterpriseMetadata interface
+type EnterpriseMetadata interface {
+	ToEnterprisePolicyMeta() *EnterprisePolicyMeta
+	EstimateSize() int
+	AddToHash(_ hash.Hash, _ bool)
+	PartitionOrDefault() string
+	PartitionOrEmpty() string
+	InDefaultPartition() bool
+	NamespaceOrDefault() string
+	NamespaceOrEmpty() string
+	InDefaultNamespace() bool
+	Merge(_ EnterpriseMeta)
+	MergeNoWildcard(_ EnterpriseMeta)
+	Normalize()
+	NormalizePartition()
+	NormalizeNamespace()
+	Matches(_ EnterpriseMeta) bool
+	IsSame(_ EnterpriseMeta) bool
+	LessThan(_ EnterpriseMeta) bool
+	WithWildcardNamespace() EnterpriseMeta
+	UnsetPartition()
+	OverridePartition(_ string)
+	FillAuthzContext(_ *AuthorizerContext)
+}
