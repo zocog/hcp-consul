c106f40f2a9eff7adff0afde280e96a428673c02
John Murret <john.murret@hashicorp.com>
2023-03-23 12:52:34 -0600

    WIP

--- .github/scripts/setup_matrix_packages.sh
+++ .github/scripts/setup_matrix_packages.sh
@@ -1,13 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
-
+GITHUB_OUTPUT=
 # set matrix var to list of unique packages containing tests
-matrix="$(go list -json="ImportPath,TestGoFiles" ./... | jq --compact-output '. | select(.TestGoFiles != null) | .ImportPath' | jq --slurp --compact-output '.')"
+matrix="$(go list -json="ImportPath,TestGoFiles" ./... | jq --compact-output '. | select(.TestGoFiles != null) | .ImportPath' | jq --slurp --compact-output '.'))"
 
-#!/usr/bin/env bash
-set -euo pipefail
-
-# set matrix var to list of unique packages containing tests
-matrix="$(go list -json="ImportPath,TestGoFiles" ./... | jq --compact-output '. | select(.TestGoFiles != null) | .ImportPath' | jq --slurp --compact-output '.')"
-
+# splitMatrix=$("jq -nc '_nwise($matrix;3)'")
 echo "matrix=${matrix}" | tee -a "${GITHUB_OUTPUT}"
