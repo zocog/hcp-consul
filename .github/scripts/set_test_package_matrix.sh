3e95eadb1820c02063b3a540b771214f6dbfdd0e
John Murret <john.murret@hashicorp.com>
2023-03-23 23:18:56 -0600

    tab separated? 2

--- .github/scripts/set_test_package_matrix.sh
+++ .github/scripts/set_test_package_matrix.sh
@@ -5,4 +5,4 @@ export RUNNER_COUNT=$1
 # set matrix var to list of unique packages containing tests
 matrix="$(go list -json="ImportPath,TestGoFiles" ./... | jq --compact-output '. | select(.TestGoFiles != null) | .ImportPath' | jq --slurp --compact-output '.' | jq --argjson runnercount $RUNNER_COUNT  -cM '[_nwise(length / $runnercount | floor)]'))"
 
-echo "matrix=${matrix}" | tee -a "${GITHUB_OUTPUT}"
+echo "matrix=${matrix}" >> "${GITHUB_OUTPUT}"
