c55e5adc0549ddedf393eb27e9844c731a83fb8a
John Eikenberry <jae@zhar.net>
2023-05-04 15:41:22 -0700

    integrate restart and acl tests into matrix

--- test/integration/vault/matrix_test.go
+++ test/integration/vault/matrix_test.go
@@ -2,8 +2,19 @@ package test
 
 import (
 	"fmt"
+	"reflect"
+	"runtime"
 	"testing"
 )
+
+// tests to run in matrix
+type testFunc func(*testing.T, TestConsulServer, TestVaultServer)
+
+var matrixTests = []testFunc{
+	tutorial,
+	testConsulManagedACLs,
+	testVaultManagedACLs,
+}
 
 // The matrix test
 func TestMatrix(t *testing.T) {
@@ -13,13 +24,33 @@ func TestMatrix(t *testing.T) {
 		if !more {
 			return
 		}
-		t.Run(fmt.Sprintf("demo-consul_%v-vault_%v", consul.version, vault.version),
-			func(t *testing.T) {
-				tutorial(t, consul, vault)
+		for _, test := range matrixTests {
+			funName := runtime.FuncForPC(reflect.ValueOf(test).Pointer()).Name()
+			testName := fmt.Sprintf("%s-consul_%v-vault_%v",
+				funName, consul.version, vault.version)
+			t.Run(testName, func(t *testing.T) {
+				test(t, consul, vault)
 			})
+			consul, vault = maybeRestart(t, consul, vault)
+		}
 		consul.Stop()
 		vault.Stop()
 	}
+}
+
+func maybeRestart(t *testing.T, c TestConsulServer, v TestVaultServer) (TestConsulServer, TestVaultServer) {
+	if c.needsRestart() {
+		t.Log("Restarting Consul")
+		c.Stop()
+		c = NewTestConsulServer(t, "consul", "local")
+	}
+
+	if v.needsRestart() {
+		t.Log("Restarting Vault")
+		v.Stop()
+		v = NewTestVaultServer(t, "vault", "local")
+	}
+	return c, v
 }
 
 // organizes matrix tests between 2 products
