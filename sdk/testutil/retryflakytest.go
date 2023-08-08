caa1da755036444c0b122910c3a3c9b187b36a04
Nick Irvine <115657443+nfi-hashicorp@users.noreply.github.com>
2023-08-08 11:11:12 -0700

    retry inside t.Run

--- sdk/testutil/retryflakytest.go
+++ sdk/testutil/retryflakytest.go
@@ -14,7 +14,14 @@ func (f *Forever) Continue() bool {
 
 // RetryFlakyTest retries a flaky test forever
 func RetryFlakyTest(t *testing.T, f func()) {
+	i := 0
 	retry.RunWith(&Forever{}, t, func(_ *retry.R) {
-		f()
+		t.Run("rerun", func(_ *testing.T) {
+			if i > 0 {
+				t.Logf("RetryFlakyTest: %d", i)
+			}
+			f()
+			i++
+		})
 	})
 }
