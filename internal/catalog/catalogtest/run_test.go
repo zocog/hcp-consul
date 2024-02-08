1f17cd3e361593e9891434ee4366038c520827ba
Matt Keeler <mjkeeler7@gmail.com>
2024-02-08 12:56:12 -0500

    Move pbresource to pbresource/v1
    # Conflicts:
    #	docs/v2-architecture/controller-architecture/testing.md
    #	internal/mesh/internal/controllers/apigateways/controller.go
    #	internal/mesh/internal/controllers/apigateways/controller_test.go
    #	internal/mesh/internal/controllers/apigateways/fetcher/data_fetcher.go
    #	internal/mesh/internal/controllers/apigateways/fetcher/data_fetcher_test.go
    #	internal/mesh/internal/controllers/gatewayproxy/controller_test.go
    #	internal/mesh/internal/controllers/gatewayproxy/mapper/meshgatewayworkloads.go
    #	proto-public/pbcatalog/v2beta1/computed_failover_policy_extras.go
    #	proto-public/pbcatalog/v2beta1/computed_failover_policy_extras_test.go

--- internal/catalog/catalogtest/run_test.go
+++ internal/catalog/catalogtest/run_test.go
@@ -10,7 +10,7 @@ import (
 	"github.com/hashicorp/consul/internal/controller/controllertest"
 	"github.com/hashicorp/consul/internal/resource/reaper"
 	rtest "github.com/hashicorp/consul/internal/resource/resourcetest"
-	"github.com/hashicorp/consul/proto-public/pbresource"
+	pbresource "github.com/hashicorp/consul/proto-public/pbresource/v1"
 )
 
 var (
